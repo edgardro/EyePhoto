86c14073c26b124962caae7375ff2bdc2af508cb
Edgard Rodriguez <edgardro@gmail.com>
2015-08-03 02:00:36 -0400

    modal css

--- platforms/ios/www/app.js
+++ platforms/ios/www/app.js
@@ -1,4 +1,4 @@
-angular.module('galleryApp',[]).
+angular.module('galleryApp',["ui.bootstrap"]).
     factory('DataSource', ['$http',function($http){
        return {
            get: function(fileName,callback){
@@ -10,7 +10,7 @@ angular.module('galleryApp',[]).
        };
     }]);
 
-var GalleryController = function($scope,DataSource) {
+var GalleryController = function($scope, $modal, DataSource) {
     var IMAGE_WIDTH = 405;
     $scope.IMAGE_LOCATION = "http://54.148.69.207/images/EyeGirls/";
     
@@ -18,14 +18,92 @@ var GalleryController = function($scope,DataSource) {
     DataSource.get("js/girlsnew.json",function(data) {
         $scope.galleryData = data;
         $scope.selected = data[0];
+
+
+           // initial image index
+     $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex < $scope.galleryData.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.galleryData.length - 1;
+        };
     });
     
+
+  //   $scope.showImages = function(index) {
+  // $scope.activeSlide = index;
+  // $scope.showModal('jsonartnewphoto1.html');
+
+ $scope.showImages = function (index) {
+    var modalInstance = $modal.open({
+      templateUrl: 'jsonartnewphoto1.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope,
+      // resolve: {
+      //    item: function () {
+      //       return item;
+      //    }
+      // }
+    });
+  };
+
+var ModalInstanceCtrl = function ($scope, $modalInstance) {
+// $scope.item = index;
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+  
+};
+
+};
+
+
+
+
     // Scroll to appropriate position based on image index and width
-    $scope.scrollTo = function(image,ind) {
-        $scope.listposition = {left:(IMAGE_WIDTH * ind * -1) + "px"};
-        $scope.selected = image;
-    };
-    $scope.go = function ( path ) {
-  $location.path( path );
-};
-};
+    // $scope.scrollTo = function(image,ind) {
+    //     $scope.listposition = {left:(IMAGE_WIDTH * ind * -1) + "px"};
+    //     $scope.selected = image;
+    // };
+//     $scope.go = function ( path ) {
+//   $location.path( path );
+// };
+
+
+// var ModalDemoCtrl = function ($scope, $modal, $log) {
+
+//   $scope.showImages = function (item) {
+//     var modalInstance = $modal.open({
+//       templateUrl: 'jsonartnewphoto1.html',
+//       controller: ModalInstanceCtrl,
+//       scope: $scope,
+//       resolve: {
+//          item: function () {
+//             return item;
+//          }
+//       }
+//     });
+//   };
+  
+// };
+
+// var ModalInstanceCtrl = function ($scope, $modalInstance, item) {
+// $scope.item = item;
+
+//   $scope.cancel = function () {
+//     $modalInstance.dismiss('cancel');
+//   };
+  
+// };
